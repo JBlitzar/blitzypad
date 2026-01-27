c364f3f8e92c8ed621295f917fc2f54d94897211
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-01-26 19:26:32 -0800

    refactor, works fr

--- firmware/main.py
+++ firmware/main.py
@@ -1,141 +1,142 @@
 import time
 
-time.sleep(3)
 import board
-from kmk.extensions.display.ssd1306 import SSD1306
-from kmk.extensions.display import Display, TextEntry
-from kmk.keys import KC
-from kmk.kmk_keyboard import KMKKeyboard
 import busio
-from kmk.scanners.keypad import KeysScanner
-from kmk.modules.macros import Press, Release, Tap, Macros
-import neopixel
-from kmk.modules.encoder import EncoderHandler
-from kmk.extensions.media_keys import MediaKeys
-import board, busio, displayio, os, terminalio
+import digitalio
+import displayio
+import terminalio
+import usb_hid
+
 import adafruit_displayio_ssd1306
-from adafruit_display_text import label
 import i2cdisplaybus
 from adafruit_display_text import bitmap_label
-from kmk.modules import Module
-import digitalio
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+try:
+    from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+except ImportError:
+    KeyboardLayoutUS = None
 
 
-btn_tl = board.D7  # SW1 - GPIO1/RX
-btn_tr = board.D8  # SW2 - GPIO2/SCK
-btn_bl = board.D9  # SW3 - GPIO4/MISO
-btn_br = board.D10  # SW4 - GPIO3/MOSI
-
-extra_btn = board.D3  # SW5 - GPIO29/ADC3/A3
-
-# rot_left = board.A1
-# rot_right = board.A0
-# rot_press = board.A3
-
-# PINS = [btn_tl, btn_tr, btn_bl, btn_br, extra_btn, rot_left, rot_right]
+# time.sleep(3)
 
 
-PINS = [btn_tl, btn_tr, btn_bl, btn_br, extra_btn]
+class DebouncedInput:
+    def __init__(self, pin: digitalio.DigitalInOut, debounce_s: float = 0.02):
+        self.pin = pin
+        self.debounce_s = debounce_s
+        self._last_raw = pin.value
+        self.value = pin.value
+        self._last_change_t = time.monotonic()
+
+    def update(self, now: float) -> bool:
+        raw = self.pin.value
+        if raw != self._last_raw:
+            self._last_raw = raw
+            self._last_change_t = now
+        if (now - self._last_change_t) >= self.debounce_s and raw != self.value:
+            self.value = raw
+            return True
+        return False
 
 
-class RotaryAsKeys(Module):
-    def __init__(self):
-        self.pins_initialized = False
+class RotaryEncoder:
+    _TRANSITIONS = {
+        (0b00, 0b01): +1,
+        (0b01, 0b11): +1,
+        (0b11, 0b10): +1,
+        (0b10, 0b00): +1,
+        (0b00, 0b10): -1,
+        (0b10, 0b11): -1,
+        (0b11, 0b01): -1,
+        (0b01, 0b00): -1,
+    }
 
-    def after_matrix_scan(self, keyboard):
-        if not self.pins_initialized:
-            import digitalio
-            import board
+    def __init__(
+        self,
+        a: digitalio.DigitalInOut,
+        b: digitalio.DigitalInOut,
+        steps_per_detent: int = 4,
+    ):
+        self.a = a
+        self.b = b
+        self.steps_per_detent = steps_per_detent
+        self._prev = self._read_state()
+        self._accum = 0
 
-            self.a = digitalio.DigitalInOut(board.A1)
-            self.a.direction = digitalio.Direction.INPUT
-            self.a.pull = digitalio.Pull.UP
+    def _read_state(self) -> int:
+        return ((1 if self.a.value else 0) << 1) | (1 if self.b.value else 0)
 
-            self.b = digitalio.DigitalInOut(board.A0)
-            self.b.direction = digitalio.Direction.INPUT
-            self.b.pull = digitalio.Pull.UP
-
-            self.sw = digitalio.DigitalInOut(board.A3)
-            self.sw.direction = digitalio.Direction.INPUT
-            self.sw.pull = digitalio.Pull.UP
-
-            self.pins_initialized = True
-            return  # wait for next scan to poll
-        if self.a.value != self.last_a:
-            if self.a.value == False:  # falling edge
-                if self.b.value == True:
-                    keyboard.press(KC.VOLD)
-                    keyboard.release(KC.VOLD)
-                else:
-                    keyboard.press(KC.VOLU)
-                    keyboard.release(KC.VOLU)
-        if self.sw.value != self.last_sw:
-            if self.sw.value == False:  # falling edge
-                keyboard.press(KC.MUTE)
-                keyboard.release(KC.MUTE)
-        self.last_a = self.a.value
-        self.last_b = self.b.value
-        self.last_sw = self.sw.value
-
-
-#
-
-
-keyboard = KMKKeyboard()
-keyboard.pixels = None
-
-keyboard.extensions.append(MediaKeys())
-
-# ENC_A = board.A1  # GPIO27
-# ENC_B = board.A0  # GPIO26
-# ENC_SW = board.A3  # GPIO28
-
-# encoder = EncoderHandler()
-# encoder.pins = ((ENC_A, ENC_B, ENC_SW),)
-# encoder.map = ((KC.A, KC.B, KC.C),)
-
-# keyboard.modules.append(encoder)
+    def update(self) -> int:
+        cur = self._read_state()
+        if cur == self._prev:
+            return 0
+        step = self._TRANSITIONS.get((self._prev, cur), 0)
+        self._prev = cur
+        if step == 0:
+            return 0
+        self._accum += step
+        if self._accum >= self.steps_per_detent:
+            self._accum = 0
+            return +1
+        if self._accum <= -self.steps_per_detent:
+            self._accum = 0
+            return -1
+        return 0
 
 
-pixels = neopixel.NeoPixel(board.D0, 2, brightness=0.3, auto_write=False)
+def _make_input(pin_id) -> digitalio.DigitalInOut:
+    pin = digitalio.DigitalInOut(pin_id)
+    pin.direction = digitalio.Direction.INPUT
+    pin.pull = digitalio.Pull.UP
+    return pin
 
 
-macros = Macros()
-keyboard.modules.append(macros)
+keyboard = Keyboard(usb_hid.devices)
+consumer = ConsumerControl(usb_hid.devices)
+layout = KeyboardLayoutUS(keyboard) if KeyboardLayoutUS else None
 
-KEYMAP = [
-    KC.W,
-    KC.D,
-    KC.A,
-    KC.S,
-    KC.MACRO("wow!"),
-    # KC.VOLD,  # rot_left
-    # KC.VOLU,  # rot_right
-    # KC.MUTE,  # rot_press
+btn_tl = DebouncedInput(_make_input(board.D7))
+btn_tr = DebouncedInput(_make_input(board.D8))
+btn_bl = DebouncedInput(_make_input(board.D9))
+btn_br = DebouncedInput(_make_input(board.D10))
+extra_btn = DebouncedInput(_make_input(board.D3))
+
+enc_a = _make_input(board.A1)
+enc_b = _make_input(board.A0)
+# enc_sw = DebouncedInput(_make_input(board.A3))
+encoder = RotaryEncoder(enc_a, enc_b)
+
+buttons = [
+    (btn_tl, Keycode.W),
+    (btn_tr, Keycode.D),
+    (btn_bl, Keycode.A),
+    (btn_br, Keycode.S),
 ]
 
-keyboard.matrix = KeysScanner(
-    pins=PINS,
-    value_when_pressed=False,
-)
 
-keyboard.keymap = [KEYMAP]
+# try:
+#     import neopixel
+#     pixels = neopixel.NeoPixel(board.D0, 2, brightness=0.3, auto_write=True)
+#     pixels.fill((0, 0, 20))
+# except ImportError:
+#     pixels = None
 
-keyboard.modules.append(RotaryAsKeys())
 
 sda, scl = board.SDA, board.SCL
-
 i2c = busio.I2C(scl, sda)
 display_bus = i2cdisplaybus.I2CDisplayBus(i2c, device_address=0x3C)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
 lbl = bitmap_label.Label(
     terminalio.FONT,
-    text="\nhello world\n",
+    text="\nready\n",
     color=0xFFFFFF,
-    label_direction="UPD",  # upside down
+    label_direction="UPD",
 )
-
 lbl.anchor_point = (0.5, 0.5)
 lbl.scale = 2
 lbl.anchored_position = (display.width // 2, display.height // 2)
@@ -145,4 +146,39 @@ group.append(lbl)
 display.root_group = group
 
 
-keyboard.go()
+def _set_status(text: str) -> None:
+    try:
+        lbl.text = f"\n{text}\n"
+    except Exception:
+        pass
+
+
+while True:
+    now = time.monotonic()
+    for btn, keycode in buttons:
+        if btn.update(now):
+            if btn.value is False:
+                keyboard.press(keycode)
+            else:
+                keyboard.release(keycode)
+    if extra_btn.update(now) and extra_btn.value is False:
+        if layout:
+            layout.write("wow!")
+        else:
+            for kc in (Keycode.W, Keycode.O, Keycode.W):
+                keyboard.send(kc)
+            keyboard.press(Keycode.SHIFT)
+            keyboard.send(Keycode.ONE)
+            keyboard.release(Keycode.SHIFT)
+        _set_status("wow!")
+    detent = encoder.update()
+    if detent == +1:
+        consumer.send(ConsumerControlCode.VOLUME_INCREMENT)
+        _set_status("vol+")
+    elif detent == -1:
+        consumer.send(ConsumerControlCode.VOLUME_DECREMENT)
+        _set_status("vol-")
+    # if enc_sw.update(now) and enc_sw.value is False:
+    #     consumer.send(ConsumerControlCode.MUTE)
+    #     _set_status("mute")
+    time.sleep(0.001)
